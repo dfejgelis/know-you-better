66d9ee099fe78765e104f61fd6c85da2fc7717ae
Diego Fejgelis <diegofej@gmail.com>
2016-05-03 17:06:25 -0300

    Eslint :: no-var

--- to_delete/test/support/compiler.js
+++ to_delete/test/support/compiler.js
@@ -1,18 +1,17 @@
-var fs = require('fs')
-var path = require('path')
-var babel = require('babel-core')
-var origJs = require.extensions['.js']
+import fs from 'fs'
+import path from 'path'
+import babel from 'babel-core'
+// var origJs = require.extensions['.js']
 
 require.extensions['.js'] = function (module, fileName) {
-  var output
   if (fileName === '/app/node_modules/react-native/Libraries/react-native/react-native.js') {
     fileName = path.resolve('./test/support/mocks/react-native.js')
   }
-  if (fileName.indexOf('node_modules/') >= 0) {
-    return (origJs || require.extensions['.js'])(module, fileName)
-  }
-  var src = fs.readFileSync(fileName, 'utf8')
-  output = babel.transform(src, {
+  // if (fileName.indexOf('node_modules/') >= 0) {
+  //   return (origJs || require.extensions['.js'])(module, fileName)
+  // }
+  const src = fs.readFileSync(fileName, 'utf8')
+  let output = babel.transform(src, {
     filename: fileName,
     sourceFileName: fileName,
     presets: ['react-native'],
