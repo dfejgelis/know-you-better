7dee04d090f4b55a9c2f2d2332470537cb34dd2e
Diego Fejgelis <diegofej@gmail.com>
2016-05-05 17:48:48 -0300

    Track :: Also include artist name

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -6,14 +6,15 @@ import styles from './TrackStyles'
 class Track extends React.Component {
   render() {
     const image = this.props.album.images[2]
+    const artist = this.props.artists[0]
 
     return (
       <React.View style={styles.container}>
         <React.Image
           source={{uri: image.url}}
           style={styles.image}
-        />
-        <React.Text style={styles.name}>{this.props.name}</React.Text>
+          />
+        <React.Text style={styles.name}>{artist.name}-{this.props.name}</React.Text>
       </React.View>
     )
   }
